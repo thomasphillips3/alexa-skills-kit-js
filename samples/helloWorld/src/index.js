0ea8d8e2a5351fa4ca976d3c244d679727a59b98
Thomas Phillips <tpphilli@amazon.com>
2016-03-25 20:07:48 -0700

    Added appId

--- samples/helloWorld/src/index.js
+++ samples/helloWorld/src/index.js
@@ -21,7 +21,7 @@
 /**
  * App ID for the skill
  */
-var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID = amzn1.echo-sdk-ams.app.8ee65bdf-350b-4afe-8547-2b0c5cf6b67e; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 
 /**
  * The AlexaSkill prototype and helper functions
